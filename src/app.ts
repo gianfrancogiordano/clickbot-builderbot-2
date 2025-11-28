95942d65276ae067b1a7bf00fa29971a410b4140
Gianfrano Giordano <gianfranogiordano@gmail.com>
2025-11-28 14:51:29 -0400

    modificaciones en el robot de notificaciones

--- src/app.ts
+++ src/app.ts
@@ -1,163 +1,60 @@
-import dotenv from 'dotenv';
-dotenv.config();
 
-import axios from 'axios';
-import { createBot, createProvider, createFlow, addKeyword, utils, EVENTS } from '@builderbot/bot'
-import { MongoAdapter as Database } from '@builderbot/database-mongo'
+import { createBot, createProvider, createFlow } from '@builderbot/bot'
+import { MemoryDB as Database } from '@builderbot/bot'
 import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
 
-
 const PORT = process.env.PORT ?? 3008
-const endpoint = `https://elb.soyclickstore.com/api/v1`;
-
-const confirmPedido = (phone: any, data?: string) => {
-    return axios.put(`${endpoint}/pedidos/bot-confirm/${phone}`, { data }, { headers: { 'Content-Type': 'application/json' } })
-}
-
-const flowWelcome = addKeyword(EVENTS.WELCOME)
-    .addAnswer(`... Estoy procesando la información`, { delay: 1000 },
-        async (ctx, { fallBack, gotoFlow, flowDynamic }) => {
-
-            try {
-                
-            if (!["1", "2"].includes(ctx.body)) {
-                return fallBack(
-                    `🤖 Lo siento, no entendí tu mensaje. Para poder ayudarte de la mejor manera, por favor intenta lo siguiente:
-
-1️⃣ Confirmar pedido ✅
-2️⃣ Modificar/Agregar Datos 📦
-                    `
-                );
-            }
-
-            switch (ctx.body) {
-                case "1":
-                    const res = await confirmPedido(ctx.from);
-                    const pedido = res.data.body;
-                    return await flowDynamic(
-                        `¡Gracias por confirmar tu pedido, ${pedido.cliente.nombre.trim()}! 🎉 
-
-Tu pedido ha sido procesado. Pronto recibirás un mensaje con el número de seguimiento.
-
-¿Necesitas asistencia *HUMANA*?
-Nos puedes contactar a través de WhatsApp:
-
-📱 Whatsapp: https://wa.me/${pedido.wsnumber}
-
-Estamos aquí para ayudarte en todo lo que necesites. 
-
-*${pedido.negocio.nombre_comercial.trim()}*.`,
-                            { delay: 1000 })
-
-                case "2":
-                    return await gotoFlow(flowEditar);
-
-            }
-
-            } catch (error) {
-                console.log(error);
-                return await flowDynamic(`Hola, ${ctx.name}. 🤖 Lo siento, ocurrió un error. Intenta nuevamente.`, { delay: 1000 })
-            }
-
-        }
-    )
-const flowEditar = addKeyword(EVENTS.ACTION)
-    .addAnswer(`Entendido, por favor, escribe en un solo mensaje qué dato deseas modificar o agregar (por ejemplo, dirección de envio, agregar otro número de contácto o agregar algún comentario). 
-
-⚠️ Si estás modificando la dirección de envío, no olvides incluir el *BARRIO, CIUDAD y DEPARTAMENTO* para evitar retrasos en la entrega. ⚠️`, { delay: 1000, capture: true },
-
-        async (ctx, { flowDynamic }) => {
-
-            try {
-            const res = await confirmPedido(ctx.from, ctx.body);
-            const pedido = res.data.body;
-            return await flowDynamic(`¡Gracias por confirmar tu pedido, ${pedido.cliente.nombre.trim()}! 🎉
-
-Tu pedido ha sido procesado. Pronto recibirás un mensaje con el número de seguimiento.
-
-¿Necesitas asistencia *HUMANA*?
-Nos puedes contactar a través de WhatsApp:
-
-📱 Whatsapp: https://wa.me/${pedido.wsnumber}
-
-Estamos aquí para ayudarte en todo lo que necesites. 
-
-*${pedido.negocio.nombre_comercial.trim()}*.`,
-                        { delay: 1000 })
-
-            } catch (error) {
-                console.log(error);
-                return await flowDynamic(`Hola, ${ctx.name}. 🤖 Lo siento, ocurrió un error. Intenta nuevamente.`, { delay: 1000 })
-            }
-
-        }
-    )
-
-const flowMedia = addKeyword(EVENTS.MEDIA)
-    .addAnswer(`... Estoy procesando la información`, { delay: 1000 },
-        async (ctx, { flowDynamic }) => {
-            
-            try {
-                return await flowDynamic(`Hola, ${ctx.name}. 🤖 Actualmente no puedo procesar imágenes o videos`, { delay: 1000 })
-            } catch (error) {
-                console.log(error);
-                return await flowDynamic(`Hola, ${ctx.name}. 🤖 Lo siento, ocurrió un error. Intenta nuevamente.`, { delay: 1000 })
-            }
-
-        }
-    )
-
-const flowVoice = addKeyword(EVENTS.VOICE_NOTE)
-    .addAnswer(`... Estoy procesando la información`, { delay: 1000 },
-        async (ctx, { flowDynamic }) => {
-
-            try {
-                return await flowDynamic(`Hola, ${ctx.name}. 🤖 En este momento, no puedo entender notas de voz.`, { delay: 1000 })
-            } catch (error) {
-                console.log(error);
-                return await flowDynamic(`Hola, ${ctx.name}. 🤖 Lo siento, ocurrió un error. Intenta nuevamente.`, { delay: 1000 })
-            }
-
-        }
-    )
 
 const main = async () => {
-    const adapterFlow = createFlow([flowWelcome, flowMedia, flowVoice, flowEditar])
-
-    const provider = createProvider(Provider, {
-        groupsIgnore: true,
-        readStatus: false,
-        usePairingCode: false,
-        experimentalStore: true,  // Reduce significativamente el consumo de recursos
-        timeRelease: 10800000,    // Limpieza de datos cada 3 horas (en milisegundos)
-        version: [2, 3000, 1025190524],  // Versión fija del protocolo que WhatsApp Web acepta
-    });
     
-    const adapterDB = new Database({
-        dbUri: process.env.MONGO_DB_URI,
-        dbName: process.env.MONGO_DB_NAME,
-    })
+    const adapterFlow = createFlow([])
 
-    const { handleCtx, httpServer } = await createBot({
+    const adapterProvider = createProvider(Provider,
+        { version: [2, 3000, 1027934701] as any }
+    )
+    
+    const adapterDB = new Database()
+
+    // const { handleCtx, httpServer } = await createBot({
+    const bot = await createBot({
         flow: adapterFlow,
-        provider: provider,
+        provider: adapterProvider,
         database: adapterDB,
     })
 
-    provider.server.post('/send-message', handleCtx(async (bot, req: any, res) => {
+    adapterProvider.server.post(
+        '/v1/messages',
+        bot.handleCtx(async (bot, req, res) => {
 
-        try {
+            console.log(req.body);
 
-            await bot?.sendMessage(req.body.phone, req.body.message, { media: req.body.mediaUrl ?? null });
-            res.end('WSSend');
+            const { number, message, urlMedia } = req.body
+            await bot.sendMessage(number, message, { media: urlMedia ?? null })
+            return res.end('sended')
+        })
+    )
 
-        } catch (error) {
-            res.end('WSError - ' + error);
-        }
+    adapterProvider.server.post(
+        '/v1/blacklist',
+        bot.handleCtx(async (bot, req, res) => {
+            const { number, intent } = req.body
+            if (intent === 'remove') bot.blacklist.remove(number)
+            if (intent === 'add') bot.blacklist.add(number)
 
-    }));
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            return res.end(JSON.stringify({ status: 'ok', number, intent }))
+        })
+    )
 
-    httpServer(+PORT)
+    bot.httpServer(+PORT)
+
+    adapterProvider.on('message', ({ body, from }) => {
+        // console.log(`Message Payload:`, { body, from })
+    })
+
+    bot.on('send_message', ({ answer, from }) => {
+        // console.log(`Send Message Payload:`, { answer, from })
+    })
 }
 
 main()
